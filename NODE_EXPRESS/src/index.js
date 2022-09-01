539bf239a04a099807110a3f33e518d876223473
Bilica <101262548+BilicaGabriella@users.noreply.github.com>
2022-09-01 09:02:18 -0300

    Add files via upload

---
+++ NODE_EXPRESS/src/index.js
@@ -0,0 +1,11 @@
+const {request, response} = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('<h1 style="color:red">Ola Express!!!</h1>')
+
+});
+
+app.listen(3000, () => console.log('Server started at localhost:3000'));
